c573186dce4c551eee4786437e33fbfb6a1e52d0
zhangcan <609884935@qq.com>
2016-05-23 16:27:01 +0800

    搭建了基本的主体框架

---
+++ BanTang/BanTang/Classes/Main/View/BTTabBar.swift
@@ -0,0 +1,21 @@
+//
+//  BTTabBar.swift
+//  BanTang
+//
+//  Created by 张灿 on 16/5/23.
+//  Copyright © 2016年 张灿. All rights reserved.
+//
+
+import UIKit
+
+class BTTabBar: UITabBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
