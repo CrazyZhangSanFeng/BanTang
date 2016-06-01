f447ec4b4954fec7bf1aeb90da52f475d4fd55e9
zhangcan <609884935@qq.com>
2016-06-01 23:57:28 +0800

    111

--- BanTang/BanTang/Classes/Home/Controller/BTHomeViewController.swift
+++ BanTang/BanTang/Classes/Home/Controller/BTHomeViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import SDCycleScrollView
+
+private let collCellID = "collCellID"
 class BTHomeViewController: UITableViewController {
    
-    
-    
+    var collectionView: UICollectionView?
+    var titleView: UIScrollView?
+    var titleButtons: [UIButton]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,10 @@ class BTHomeViewController: UITableViewController {
         
         self.automaticallyAdjustsScrollViewInsets = false
         
+        tableView.scrollsToTop = false
+        
+        //添加所有子控制器
+        setupAllChildVC()
         
         
     }
@@ -73,9 +80,9 @@ extension BTHomeViewController: SDCycleScrollViewDelegate {
 //MARK:- 设置滚动标题栏
 extension BTHomeViewController {
     override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let titleView = UIScrollView()
-        titleView.backgroundColor = UIColor.orangeColor()
-        
+        titleView = UIScrollView()
+        titleView!.backgroundColor = UIColor.orangeColor()
+        titleView!.scrollsToTop = false
         return titleView
     }
     
@@ -84,7 +91,7 @@ extension BTHomeViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return 1
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -100,9 +107,13 @@ extension BTHomeViewController {
         }
         
         // 3.给cell设置数据
-        cell?.textLabel?.text = "测试数据:\(indexPath.row)"
+        cell?.contentView.addSubview(setupBottomView())
         
         return cell!
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UIScreen.mainScreen().bounds.height
     }
     
     
@@ -129,3 +140,136 @@ extension BTHomeViewController {
     }
     
 }
+
+ //MARK:- 创建底部视图
+extension BTHomeViewController {
+    func setupBottomView() -> UICollectionView {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.itemSize = CGSize(width: UIScreen.mainScreen().bounds.width, height: UIScreen.mainScreen().bounds.height)
+        layout.scrollDirection = .Horizontal
+        
+        
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: UIScreen.mainScreen().bounds.width, height: UIScreen.mainScreen().bounds.height) , collectionViewLayout: layout)
+        
+        collectionView!.scrollsToTop = false
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        collectionView!.pagingEnabled = true
+        collectionView!.showsHorizontalScrollIndicator = false
+        
+        collectionView!.registerClass(NSClassFromString("UICollectionViewCell"), forCellWithReuseIdentifier: collCellID)
+        
+        return collectionView!
+    }
+}
+
+//MARK:- collectionView数据源,代理
+extension BTHomeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.childViewControllers.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(collCellID, forIndexPath: indexPath)
+        //移除之前的所有控制器
+        cell.contentView.subviews.map{
+            $0.removeFromSuperview()
+        }
+        //获取相对应的控制器
+        let vc = self.childViewControllers[indexPath.row] 
+        
+        vc.view.frame = CGRect(x: 0, y: 0, width: UIScreen.mainScreen().bounds.width, height: UIScreen.mainScreen().bounds.height)
+        cell.contentView.addSubview(vc.view)
+        
+        return cell
+    }
+
+}
+
+//MARK:- 添加所有子控制器
+extension BTHomeViewController {
+    
+    func setupAllChildVC() {
+        //最新
+        let newVC = UITableViewController()
+        newVC.title = "最新"
+        self.addChildViewController(newVC)
+        
+        //用户原创
+        let userVC = UITableViewController()
+        userVC.title = "用户原创"
+        self.addChildViewController(userVC)
+        
+        //一周最热
+        let hotVC = UITableViewController()
+        hotVC.title = "一周最热"
+        self.addChildViewController(hotVC)
+        
+        //美妆&穿搭
+        let wearVC = UITableViewController()
+        wearVC.title = "美妆&穿搭"
+        self.addChildViewController(wearVC)
+        
+        //美食
+        let foodVC = UITableViewController()
+        foodVC.title = "美食"
+        self.addChildViewController(foodVC)
+        
+        //设计感
+        let designVC = UITableViewController()
+        designVC.title = "设计感"
+        self.addChildViewController(designVC)
+        
+        //礼物
+        let giftVC = UITableViewController()
+        giftVC.title = "礼物"
+        self.addChildViewController(giftVC)
+        
+    }
+}
+
+//MARK:- 添加所有标题
+extension BTHomeViewController {
+    func setupAllTitle() {
+        let count = self.childViewControllers.count
+        var x: CGFloat = 0
+        let y: CGFloat = 0
+        let w: CGFloat = UIScreen.mainScreen().bounds.width / 4
+        let h: CGFloat = (titleView?.frame.size.height)!
+        
+        for i in 0..<count {
+            let vc = self.childViewControllers[i]
+            let button = UIButton(type: .Custom)
+            button.setTitle(vc.title, forState: .Normal)
+            button.tag = i
+            button.setTitleColor(UIColor.darkGrayColor(), forState: .Normal)
+            button.setTitleColor(UIColor.redColor(), forState: .Selected)
+            button.titleLabel?.font = UIFont.boldSystemFontOfSize(15)
+            x = CGFloat(i) * w
+            button.frame = CGRect(x: x, y: y, width: w, height: h)
+            button.addTarget(self, action: #selector(BTHomeViewController.buttonClick(_:)), forControlEvents: .TouchUpInside)
+            titleView?.addSubview(button)
+            titleButtons?.append(button)
+            //默认选中第一个
+            if i == 0 {
+                
+            }
+            
+            
+        }
+    }
+}
+
+//MARK:- 按钮点击
+extension BTHomeViewController {
+
+    func buttonClick(button: UIButton) {
+        
+    }
+
+}
+
+
